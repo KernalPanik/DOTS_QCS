2c1c74ab8bb22b64ee2a69a2ec658df79a4fd598
KernalPanik <lukas.michnevic@protonmail.com>
2020-10-10 21:41:32 +0300

    Implemented better version of quantum circuit system
    It is now possible to create a pipeline of gates

---
+++ DOTS_QCS/Assets/Scripts/Gates.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public enum GateCodes
+{
+    IDENTITY = 0,
+    X = 1
+}
+
+public static class Gates
+{
+    /// <summary>
+    /// Apply gate by passing it's code
+    /// </summary>
+    public static void ApplyGate(int gateCode, ref Rotation qubitRotation)
+    {
+        switch(gateCode)
+        {
+            case (int)GateCodes.IDENTITY:
+                ApplyI(ref qubitRotation);
+                break;
+            case (int)GateCodes.X:
+                ApplyX(ref qubitRotation);
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Apply identity matrix (nop)
+    /// </summary>
+    public static void ApplyI(ref Rotation qubitRotation)
+    {
+        qubitRotation.Value = math.mul(math.normalizesafe(qubitRotation.Value), quaternion.identity);
+    }
+
+    /// <summary>
+    /// Apply X gate (NOT gate)
+    /// </summary>
+    public static void ApplyX(ref Rotation qubitRotation)
+    {
+        qubitRotation.Value = math.mul(math.normalizesafe(qubitRotation.Value), quaternion.RotateX(math.PI));
+    }
+}
---
+++ DOTS_QCS/Assets/Scripts/QuantumComputer.cs
@@ -0,0 +1,211 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Jobs;
+using Unity.Collections;
+using System.Collections.Generic;
+
+public struct Energy : IComponentData
+{
+    public float Value;
+}
+
+public struct QuantumState : IComponentData
+{
+    /// <summary>
+    /// Alpha amplitude describes state |0>
+    /// Beta amplitude describes state |1>
+    /// </summary>
+    public float Alpha;
+    public float Beta;
+}
+
+public struct GateIdComponent : IComponentData
+{
+    public int Value;
+}
+
+public struct GateComponent : IComponentData
+{
+    /// <summary>
+    /// GateCode is an opcode of a gate, see the list //TODO:
+    /// QubitIds is the array of qubits a gate should be applied to
+    /// </summary>
+    public int GateCode;
+    public int QubitId;
+}
+
+public struct QubitComponent : IComponentData
+{
+    public int QubitId;
+}
+
+public struct SphericalCoords
+{
+    public float Theta;
+    public float Phi;
+}
+
+public struct SingleQubitGate : IComponentData
+{
+    public int ExecutionOrder;
+    public int GateCode;
+}
+
+public class PhysicalToQuantumSystem : ComponentSystem
+{
+    /// <summary>
+    /// Calculate quantum state amplitudes based on the rotation of the qubit
+    /// cos(theta/2)* state_0 + e^(i*fi)*sin(theta/2)* state_1
+    /// </summary>
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((Entity entity, ref Rotation rotation, ref QuantumState quantumState) => 
+        {
+            var coords = ExtraMath.QuaternionToSpherical(rotation.Value);
+            quantumState.Alpha = math.cos(coords.Theta / 2);
+            quantumState.Beta = math.sqrt(1 - math.pow(quantumState.Alpha, 2));
+        });
+    }
+}
+
+public static class ExtraMath
+{
+    /// <summary>
+    /// a function to convert a given quaternion into vector of longitude and lattitude
+    /// </summary>
+    public static SphericalCoords QuaternionToSpherical(quaternion quaternion)
+    {
+        SphericalCoords coords = new SphericalCoords();
+        //TODO: Verify if such conversion is correct
+        coords.Theta = math.atan2(quaternion.value.y, quaternion.value.w);
+        coords.Phi = math.acos(quaternion.value.z / 1); // since we're working with a unit sphere, our radius is equal to 1
+        return coords;
+    }
+}
+
+public class QuantumCircuit : ComponentSystem
+{
+    private int executedStep = 0;
+    protected override void OnUpdate()
+    {
+        //TODO: Find out how to add OR check to foreach
+
+        var em = World.DefaultGameObjectInjectionWorld.EntityManager;
+        EntityCommandBuffer cmd;
+            
+        foreach(var gate in QuantumComputer.gateList)
+        {
+            var gateComponent = em.GetComponentData<GateComponent>(gate);
+
+            // Find qubits which should be modified
+            Entities.ForEach((Entity entity, ref QuantumState quantumState, ref Rotation rotation, ref QubitComponent qubitComponent) => 
+            { 
+                if(gateComponent.QubitId == qubitComponent.QubitId)
+                {
+                    // Add singleQubitGate component to this entity
+                    // Pretty sure that ExecutionOrder is not needed in this case
+                    em.AddComponentData(entity, new SingleQubitGate { ExecutionOrder = 0, GateCode = gateComponent.GateCode });
+                }
+            });
+
+            // Go through all entities with attached SingleQubitGate components
+            Entities.ForEach((Entity entity, ref QuantumState quantumState, ref Rotation rotation,
+                ref QubitComponent qubitComponent, ref SingleQubitGate singleQubitGate) =>
+            {
+                Gates.ApplyGate(singleQubitGate.GateCode, ref rotation);
+            });
+
+            // Remove all Attached single qubit gate components 
+            Entities.ForEach((Entity entity) =>
+            {
+                //em.RemoveComponent<SingleQubitGate>(entity);
+            });
+        }
+    }
+}
+
+public class QuantumComputer : MonoBehaviour
+{
+    private EntityManager entityManager;
+    private int QubitCount = 0;
+    private int GateCount = 0;
+
+    public Dictionary<int, Entity> qubitDictionary = new Dictionary<int, Entity>();
+
+    public static List<Entity> gateList = new List<Entity>();
+
+    private void Start()
+    {
+        entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        //maybe encapsulate the dictionary?
+        qubitDictionary.Add(QubitCount, CreateQubit(entityManager));
+        qubitDictionary.Add(QubitCount, CreateQubit(entityManager));
+        qubitDictionary.Add(QubitCount, CreateQubit(entityManager));
+        qubitDictionary.Add(QubitCount, CreateQubit(entityManager));
+        qubitDictionary.Add(QubitCount, CreateQubit(entityManager));
+        qubitDictionary.Add(QubitCount, CreateQubit(entityManager));
+
+        //Apply I gate to the qubit with id 0
+        gateList.Add(CreateGate(entityManager, 0, 0));
+        gateList.Add(CreateGate(entityManager, 1, 1));
+        gateList.Add(CreateGate(entityManager, 0, 2));
+        gateList.Add(CreateGate(entityManager, 1, 3));
+        gateList.Add(CreateGate(entityManager, 0, 4));
+        gateList.Add(CreateGate(entityManager, 1, 5));
+        gateList.Add(CreateGate(entityManager, 1, 5));
+    }
+
+    public Entity CreateQubit(EntityManager entityManager)
+    {
+        EntityArchetype archetype = CreateQubitArchetype(entityManager);
+        var qubit = entityManager.CreateEntity(archetype);
+        entityManager.SetName(qubit, "qubit");
+        entityManager.AddComponentData(qubit, new QubitComponent { QubitId = QubitCount });
+        QubitCount++;
+        return qubit;
+    }
+
+    private Entity CreateGate(EntityManager entityManager)
+    {
+        EntityArchetype archetype = CreateGateArchetype(entityManager);
+        var gate = entityManager.CreateEntity(archetype);
+        entityManager.SetName(gate, "gate");
+        entityManager.AddComponentData(gate, new GateIdComponent { Value = GateCount });
+        GateCount++;
+        return gate;
+    }
+
+    public Entity CreateGate(EntityManager entityManager, int gateCode, int qubit)
+    {
+        var gate = CreateGate(entityManager);
+        entityManager.AddComponentData(gate, new GateComponent { GateCode = gateCode, QubitId = qubit});
+        return gate;
+    }
+
+    public Entity CreateGate(EntityManager entityManager, int gateCode, int[] qubits)
+    {
+        /* var gate = CreateGate(entityManager);
+         entityManager.AddComponentData(gate, new GateComponent { GateCode = gateCode, QubitId = qubits});
+         return gate;*/
+        return default;
+    }
+
+    private EntityArchetype CreateQubitArchetype(EntityManager entityManager)
+    {
+        return entityManager.CreateArchetype(
+           typeof(Translation),
+           typeof(Rotation),
+           typeof(Energy),
+           typeof(QuantumState),
+           typeof(QubitComponent));
+    }
+
+    private EntityArchetype CreateGateArchetype(EntityManager entityManager)
+    {
+        return entityManager.CreateArchetype(
+            typeof(GateIdComponent),
+            typeof(GateComponent));
+    }
+}
